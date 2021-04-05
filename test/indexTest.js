eb8966526f32b3ca2f10c6d584100a78354ecb2d
eman hamid <emanhharki@gmail.com>
2021-04-05 11:54:47 +0000

    Automatically backed up by Learn

--- test/indexTest.js
+++ test/indexTest.js
@@ -33,7 +33,7 @@ describe('index.js', function () {
 
   describe('companyCeo', function () {
     it('is declared as equal to Lauren Hart', function () {
-      expect(companyCeo).to.equal('Lauren Hart');
+      expect(companyCeo).to.equal('');
     });
 
     it('does not raise error if the companyCeo is changed', function () {
